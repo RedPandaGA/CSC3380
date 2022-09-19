908ffbc50151a03eab2667247b2317b47ed81597
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-09-19 13:39:14 -0500

    Got Router to work. Setup basic example of how it works. Wasn't working due to accidentally installing router-dom into wrong dir.

---
+++ ReactDev/pekan-pi-front/src/Components/Layout.js
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function Layout() {
+    return ( 
+        <div>
+            <h1>Layout</h1>
+            <Outlet />
+        </div>
+    );
+}
+
+export default Layout;
