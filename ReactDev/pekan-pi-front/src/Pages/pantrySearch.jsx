a57aae932050f74e8de3eaff3fa2c596dbe8a2f5
Adam Elkhanoufi <aelkha5@lsu.edu>
2022-11-09 13:41:05 -0600

    fixed pantrySearch

--- ReactDev/pekan-pi-front/src/Pages/pantrySearch.jsx
+++ ReactDev/pekan-pi-front/src/Pages/pantrySearch.jsx
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import serverAPI from "/ExpressDev/backendServer/app";
 import "./pantrySearch.css";
 import searchIcon from '@mui/icons-material/Search';
 import closeIcon from '@mui/icons-material/Close';
