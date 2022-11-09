680c65ac665ab30debdc751d9b994858a68f3100
Moroccxn <aelkha5@lsu.edu>
2022-11-08 19:15:24 -0600

    Functional Recipe Page

---
+++ ReactDev/pekan-pi-front/src/Pages/RecipiesPage.jsx
@@ -0,0 +1,34 @@
+import React, {useState, useEffect} from 'react';
+import RecipesList from './recipesList';
+//import './Recipe.css';
+
+//const APIkey = "ec6b18b84aea41b8b12f6a6b6b4cfb67";
+const ingredients = "chicken,+onions,+garlic,+pasta,+tomatoes"
+const APIkey = "5569351c7ed44a0fa6de0b81882daa58";
+
+function RecipiesPage() {
+    const [recipeData, setRecipeData] = useState(null);
+    
+    useEffect(() => {
+        getRecipe();
+    }, []);
+
+    function getRecipe() {
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIkey}&ingredients=${ingredients}`)
+        .then((response) => response.json())
+        .then((data) => {
+            setRecipeData(data);
+        })
+        .catch(() => {
+            console.log("error fetching recipe");
+        });
+    }
+
+    return ( 
+        <div className="RecipiesPage">
+            {recipeData && <RecipesList recipeData={recipeData} />}
+        </div>
+    );
+}
+
+export default RecipiesPage;
