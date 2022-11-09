05618093eef4782f8513155d8fd15bcddccae793
Milan Nguyen <m.nguyen2035@gmail.com>
2022-11-09 01:36:12 -0600

    update profile imports
    it broke before bc i merge w gavin- doubled imports

--- ReactDev/pekan-pi-front/src/Profile/Profile.jsx
+++ ReactDev/pekan-pi-front/src/Profile/Profile.jsx
@@ -13,15 +13,6 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 import Box from '@mui/material/Box';
 
-import * as React from 'react';
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
-import Box from '@mui/material/Box';
-
 const maintheme = createTheme({  // makes the theme for the whole profile
     palette: {
         primary: {
