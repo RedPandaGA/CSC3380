3043b565f0940afa37d9579a6d77d30bbe25b4fd
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2022-11-09 14:55:47 -0600

    Merge branch 'websiteFetches-FK' into BackendDev-GA

--- ReactDev/pekan-pi-front/src/App.js
+++ ReactDev/pekan-pi-front/src/App.js
@@ -3,49 +3,73 @@ import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
 import Pantry from "./Pages/Pantry";
 import Profile from "./Profile/Profile";
-import RecipiesPage from "./Pages/RecipiesPage";
+import RecipesPage from "./Pages/RecipesPage";
 import Login from "./Pages/Login";
 import Forgot from "./Pages/Forgot/Forgot";
 import "./index.css";
 
-import {useState} from 'react'
-
+import { useState } from "react";
 
 function App() {
+  const [darkmode, setDarkMode] = useState(false);
 
-  const [darkmode,setDarkMode] =useState(false);
+  function HomeElement() {
+    return (
+      <>
+        {" "}
+        <Layout darkmode={darkmode} setDarkMode={setDarkMode} />{" "}
+        <Home darkmode={darkmode} />
+      </>
+    );
+  }
+  function ProfileElement() {
+    return (
+      <>
+        {" "}
+        <Layout darkmode={darkmode} setDarkMode={setDarkMode} />{" "}
+        <Profile darkmode={darkmode} />
+      </>
+    );
+  }
+  function ForgotElement() {
+    return (
+      <>
+        {" "}
+        <Layout darkmode={darkmode} setDarkMode={setDarkMode} />{" "}
+        <Forgot darkmode={darkmode} />
+      </>
+    );
+  }
 
-  function HomeElement(){
-    return  <> <Layout darkmode={darkmode}  setDarkMode={setDarkMode} /> <Home darkmode={darkmode}  /></> 
-  }
-  function ProfileElement(){
-  
-    
-    return <> <Layout darkmode={darkmode}   setDarkMode={setDarkMode} /> <Profile darkmode={darkmode}  /></>
-  }
-  function ForgotElement(){
-    return <> <Layout darkmode={darkmode}   setDarkMode={setDarkMode}/> <Forgot darkmode={darkmode}  /></>
-  }
-  
   function RecipePage() {
-    return <> <Layout darkmode={darkmode}  setDarkMode={setDarkMode} /> <RecipiesPage darkmode={darkmode}  /></>
+    return (
+      <>
+        {" "}
+        <Layout darkmode={darkmode} setDarkMode={setDarkMode} />{" "}
+        <RecipesPage darkmode={darkmode} />
+      </>
+    );
   }
-  
+
   function PantryPage() {
-    return <> <Layout darkmode={darkmode}  setDarkMode={setDarkMode} /> <Pantry darkmode={darkmode}  /></>
+    return (
+      <>
+        {" "}
+        <Layout darkmode={darkmode} setDarkMode={setDarkMode} />{" "}
+        <Pantry darkmode={darkmode} />
+      </>
+    );
   }
 
   return (
-    <div className={`react-app ${darkmode?"darkmode-page":""}`}>
- 
-
+    <div className={`react-app ${darkmode ? "darkmode-page" : ""}`}>
       <Routes className="NavBar">
-        <Route index element={ <HomeElement /> } />
-        <Route path="/Login" element={<Login darkmode={darkmode}  />} />
-        <Route path="/Recipes" element={<RecipePage/> } />
-        <Route path="/Profile/Profile" element={ <ProfileElement/> } />
-        <Route path="/Pantry" element={<PantryPage/> } />
-        <Route path="/forgot" element={<ForgotElement /> } />
+        <Route index element={<HomeElement />} />
+        <Route path="/Login" element={<Login darkmode={darkmode} />} />
+        <Route path="/Recipes" element={<RecipePage />} />
+        <Route path="/Profile/Profile" element={<ProfileElement />} />
+        <Route path="/Pantry" element={<PantryPage />} />
+        <Route path="/forgot" element={<ForgotElement />} />
       </Routes>
 
       {/*add footer */}
