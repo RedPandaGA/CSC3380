c8061354c1df1e43aa2a5c0d6417c712747a0953
Adam Elkhanoufi <aelkha5@lsu.edu>
2022-11-09 13:39:22 -0600

    Update

--- ReactDev/pekan-pi-front/src/Pages/pantrySearch.jsx
+++ ReactDev/pekan-pi-front/src/Pages/pantrySearch.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import serverAPI from "/ExpressDev/backendServer/app";
 import "./pantrySearch.css";
 import searchIcon from '@mui/icons-material/Search';
 import closeIcon from '@mui/icons-material/Close';
