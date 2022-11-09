2bba33f3847bec0d7278770d24862d62fb07ee14
Faris Khattak <fariskhattak8@gmail.com>
2022-11-08 22:06:53 -0600

    Changed gitignore and .env

--- ReactDev/pekan-pi-front/src/Pages/RecipiesPage.jsx
+++ ReactDev/pekan-pi-front/src/Pages/RecipiesPage.jsx
@@ -1,34 +1,36 @@
-import React, {useState, useEffect} from 'react';
-import RecipesList from './recipesList';
+import React, { useState, useEffect } from "react";
+import RecipesList from "./recipesList";
 //import './Recipe.css';
 
 //const APIkey = "ec6b18b84aea41b8b12f6a6b6b4cfb67";
-const ingredients = "beef,+cheese,+garlic,+rice,+salt,+pepper,+lemons,+eggs"
-const APIkey = "5569351c7ed44a0fa6de0b81882daa58";
+const ingredients = "beef,+cheese,+garlic,+rice,+salt,+pepper,+lemons,+eggs";
+const APIkey = "d123acc25cd74b6a859c246e37ec77f6"; // Faris API key
 
 function RecipiesPage() {
-    const [recipeData, setRecipeData] = useState(null);
-    
-    useEffect(() => {
-        getRecipe();
-    }, []);
+  const [recipeData, setRecipeData] = useState(null);
 
-    function getRecipe() {
-        fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIkey}&ingredients=${ingredients}`)
-        .then((response) => response.json())
-        .then((data) => {
-            setRecipeData(data);
-        })
-        .catch(() => {
-            console.log("error fetching recipe");
-        });
-    }
+  useEffect(() => {
+    getRecipe();
+  }, []);
 
-    return ( 
-        <div className="RecipiesPage">
-            {recipeData && <RecipesList recipeData={recipeData} />}
-        </div>
-    );
+  function getRecipe() {
+    fetch(
+      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${APIkey}&ingredients=${ingredients}&number=5`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setRecipeData(data);
+      })
+      .catch(() => {
+        console.log("error fetching recipe");
+      });
+  }
+
+  return (
+    <div className="RecipiesPage">
+      {recipeData && <RecipesList recipeData={recipeData} />}
+    </div>
+  );
 }
 
 export default RecipiesPage;
